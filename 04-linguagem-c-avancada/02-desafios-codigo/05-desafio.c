146c7db33bfd9147f3c00b8f98a69d9425885f6a
brennocm <brennocmiranda@outlook.com>
2023-04-23 20:27:08 -0300

    implementa desafios

--- 04-linguagem-c-avancada/02-desafios-codigo/05-desafio.c
+++ 04-linguagem-c-avancada/02-desafios-codigo/05-desafio.c
@@ -7,53 +7,46 @@ typedef struct {
 } Cogumelo;
 
 int main() {
-    Cogumelo cogumelos[] = {
-        {"Shitake", 10},
-        {"Portobello", 8},
-        {"Shimeji", 6},
-        {"Champignon", 12},
-        {"Funghi", 16},
-        {"Porcini", 16}
-    };
+  Cogumelo cogumelos[] = {
+    {"Shitake", 10},
+    {"Portobello", 8},
+    {"Shimeji", 6},
+    {"Champignon", 12},
+    {"Funghi", 16},
+    {"Porcini", 16}
+  };
 
-    char escolha[20];
-    scanf("%19s", escolha);
+  char escolha[20];
+  scanf("%19s", escolha);
 
-    int indice_escolhido = -1;
-    for (int i = 0; i < 6; i++) {
-        if (strcmp(escolha, cogumelos[i].nome) == 0) {
-            indice_escolhido = i;
-            break;
-        }
+  int indice_escolhido = -1;
+  for (int i = 0; i < 6; i++) {
+    if (strcmp(escolha, cogumelos[i].nome) == 0) {
+      indice_escolhido = i;
+      break;
     }
+  }
 
-    Cogumelo sugestoes[2];
-    int numeroSugestoes = 0;
+  Cogumelo sugestoes[2];
+  int numeroSugestoes = 0;
 
-    // Verifica se a escolha é a última opção da lista, caso contrário, sugere os cogumelos mais baratos
-    if (indice_escolhido == 5) {
-        printf("Desculpe, não há sugestões disponíveis.\n");
-    } else {
-        // Verifica se o próximo cogumelo é mais barato
-        if (cogumelos[indice_escolhido+1].valor <= cogumelos[indice_escolhido].valor) {
-            sugestoes[numeroSugestoes] = cogumelos[indice_escolhido+1];
-            numeroSugestoes++;
-        }
-        // Verifica se o segundo próximo cogumelo é mais barato
-        if (numeroSugestoes < 2 && cogumelos[indice_escolhido+2].valor <= cogumelos[indice_escolhido].valor) {
-            sugestoes[numeroSugestoes] = cogumelos[indice_escolhido+2];
-            numeroSugestoes++;
-        }
-
-        if (numeroSugestoes == 0) {
-            printf("Desculpe, não há sugestões disponíveis.\n");
-        } else {
-            for (int i = 0; i < numeroSugestoes; i++) {
-                printf("%s - Valor: %d\n", sugestoes[i].nome, sugestoes[i].valor);
-            }
-        }
+  for (int i = 0; i < 6 && numeroSugestoes < 2; i++) {
+    if (cogumelos[i].valor <= cogumelos[indice_escolhido].valor) {
+      if (strcmp(cogumelos[i].nome, escolha) != 0) {
+        sugestoes[numeroSugestoes] = cogumelos[i];
+        numeroSugestoes++;
+      }
     }
+  }
 
-    return 0;
+  if (numeroSugestoes == 0) {
+    printf("Desculpe, não há sugestões disponíveis.");
+  } else {
+    for (int i = 0; i < numeroSugestoes; i++) {
+      printf("%s - Valor: %d\n", sugestoes[i].nome, sugestoes[i].valor);
+    }
+  }
+
+  return 0;
 }
 
